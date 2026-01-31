0acf6dc541c393c9a14cf30d51fea8101351d5e5
Deadend4 <vistanciya.toxin@mail.ru>
2026-01-31 22:57:28 +0300

    add mute mic feature

--- frontend/src/components/Avatar/Avatar.styled.ts
+++ frontend/src/components/Avatar/Avatar.styled.ts
@@ -4,9 +4,21 @@ interface AvatarProps {
   size?: number;
 }
 
+export const RelativeBlock = styled.div({
+  position: "relative",
+});
+
 export const Avatar = styled.img<AvatarProps>(({ size }) => ({
   width: size ?? "100%",
   height: size ?? "100%",
   borderRadius: "50%",
   objectFit: "cover" as const,
+}));
+
+export const MutedImg = styled.img(({ theme }) => ({
+  position: "absolute",
+  bottom: 0,
+  right: 0,
+  borderRadius: "50%",
+  backgroundColor: theme.colors.primary.light,
 }));
